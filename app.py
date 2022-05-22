4bf05fef301f0934907fe251b7928b08ef2e1301
Blablage <gergarmac@gmail.com>
2022-05-21 23:31:36 -0500

    correcciones en app.py e index.html

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import csv
 app = Flask(__name__)
 
 usuarios = [usr.usuario('Gerardo'), usr.usuario('Luis'), usr.usuario('Yeni')]
-campos = ["Usuario", "Nombre Tarjeta", "Tasa Interes", "Deuda Inicial", "Por abonar"]
 
 usuarios[0].agregar_tarjeta('credito','Uno', 5, 2000)
 usuarios[0].agregar_tarjeta('credito','Dos', 2, 8000)
@@ -18,6 +17,13 @@ usuarios[1].agregar_tarjeta('servicio', 'Tres', 3, 6000)
 usuarios[2].agregar_tarjeta('credito','Uno', 5, 10000)
 usuarios[2].agregar_tarjeta('credito','Dos', 2, 5000)
 usuarios[2].agregar_tarjeta('servicio', 'Tres', 3, 1000)
+
+#Busca a un Usuario y obtiene su posicion en la lista
+def get_index_lista_usuarios(nombre):
+  for obj in usuarios:
+    if nombre == obj.usuario:
+      return usuarios.index(obj)
+  return -1
 
 #Se pueden usar diccionarios como argumentos para writerow
 with open("usuarios.csv", 'w') as fcsv: 
@@ -32,7 +38,15 @@ with open("usuarios.csv", 'w') as fcsv:
 # Permite que la ruta funcione para peticiones GET y POST
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  
+  if request.method == "POST":
+    #print(request.form["usuario"])
+    #print(request.form["tarjeta"])
+    #print(request.form["tasa"])
+    #print(request.form["deuda"])
+
+    queryUser = get_index_lista_usuarios(request.form["usuario"])
+    #queryTarjeta = usuarios[queryUser].get_index_lista_tarjetas(request.form["tarjeta"])
+    usuarios[queryUser].multiples_reportes()
+
   with open("usuarios.csv") as file:
     return render_template("index.html", csv=file)
-  #BUscar crear un boton para crear los reportes 
